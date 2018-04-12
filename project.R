750c5d39bd7e7b461160830e8370ffecffa9ebb4
brian.weinfeld+cuny@gmail.com <brian.weinfeld+cuny@gmail.com>
2018-04-11 22:55:16 -0400

    rmd rough draft

--- project.R
+++ project.R
@@ -12,34 +12,37 @@ library(XML)
 library(magrittr)
 library(tm)
 library(SnowballC)
+library(caret)
+set.seed(1234)
 
-Website.Parse <- function(type){
-  list.files(path=paste0('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\607\\607project4\\SW\\', type, '\\'), pattern="\\d+") %>%
-  map_dfr(~getURL(paste0('file:///C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\607\\607project4\\SW\\', type, '\\', .x)) %>% 
+
+Website.Parse <- function(folder, type){
+  path <- paste0('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\607\\607project4\\', folder, '\\', type, '\\')
+  list.files(path=path, pattern="\\d+") %>%
+  map_dfr(~getURL(paste0('file:///', path, .x)) %>% 
             htmlParse() %>%
             xpathSApply('/html//body', xmlValue) %>%
             strsplit(split='\\n') %>%
             unlist() %>%
             as.tibble() %>%
-            add_column(type=paste(type, .x, sep='_'), .before=1)
+            add_column(type=type, .before=1) %>%
+            add_column(id=paste(type, .x, sep='_'), .before=1)
         )
 }
 
+custom.stopwords <- data_frame(word=c('div', 'p', 'h', 'img', 'function'))
 
 data <- c('Bands', 'BioMedical', 'Goats', 'Sheep') %>%
-  map_df(~Website.Parse(.)) %>%
+  map_df(~Website.Parse('Train', .)) %>%
   unnest_tokens(word, value) %>%
-  anti_join(stop_words)
-
+  anti_join(stop_words) %>%
+  filter(!str_detect(word, '(\\d|\\.|[^A-Za-z])+')) %>%
+  filter(stringi::stri_enc_mark(word) == 'ASCII') %>%
+  mutate(word = wordStem(word)) %>%
+  anti_join(custom.stopwords)
 
 # td_idf by topic ---------------------------------------------------------
-custom.stopwords <- data_frame(word=c('div', 'p', 'h'))
-
-tfidf <- data %>%
-  separate(type, c('type', 'document'), sep='_') %>%
-  filter(!str_detect(word, '(\\d|\\.)+')) %>%
-  mutate(word = wordStem(word)) %>%
-  anti_join(custom.stopwords) %>%
+data.tfidf <- data %>%
   count(type, word) %>%
   bind_tf_idf(word, type, n) %>%
   arrange(type, tf_idf) %>%
@@ -47,52 +50,45 @@ tfidf <- data %>%
   group_by(type) %>%
   top_n(10, tf_idf)
 
-ggplot(tfidf, aes(order, tf_idf, fill=type)) +
+ggplot(data.tfidf, aes(order, tf_idf, fill=type)) +
   geom_bar(show.legend=FALSE, stat='identity') +
   facet_wrap(~type, scales='free') +
   coord_flip() +
   theme(axis.text.x=element_text(angle=-30, vjust=1, hjust=0)) +
   scale_x_continuous(
-    breaks = tfidf$order,
-    labels = tfidf$word
+    breaks = data.tfidf$order,
+    labels = data.tfidf$word
   )
 # td_idf by topic ---------------------------------------------------------
 
+test.numbers <- sample(1:323, 32, replace=FALSE)
 
+ml.data <- data %>%
+  count(type, id, word) %>%
+  cast_dtm(id, word, n) %>%
+  removeSparseTerms(sparse=0.99) %>%
+  as.matrix() %>%
+  as.data.frame() %>%
+  mutate(type = str_extract(row.names(.), '[^_]+'))
 
+train.data <- ml.data %>%
+  filter(!(row_number() %in% test.numbers))
 
+test.data <- ml.data %>%
+  filter(row_number() %in% test.numbers)
 
+train.results <- train(type~., data=train.data,
+           method='rf',
+           trControl=trainControl(method='cv',
+                                  number = 2
+                                  ),
+           verbose=FALSE)
 
+test.results <- predict(train.results, newdata=test.data)
 
-word_counts <- data %>%
-  count(type, word, sort=TRUE) %>%
-  ungroup()
+present.results <- data_frame(original=test.data$type, prediction=test.results)
 
-chapters_dtm <- word_counts %>%
-  cast_dtm(type, word, n)
-
-chapters_lda <- LDA(chapters_dtm, k=4, control=list(seed=1234))
-
-topics <- tidy(chapters_lda, matrix='gamma')
-
-topics %>%
-  separate(document, c('type', 'document'), sep='_') %>%
-  mutate(type = reorder(type, gamma*topic)) %>%
-  ggplot(aes(factor(topic), gamma)) +
-  geom_boxplot() +
-  facet_wrap(~type)
-
-
-
-
-
-
-
-
-
-
-
-
+table(present.results$original, present.results$prediction)
 
 
 
